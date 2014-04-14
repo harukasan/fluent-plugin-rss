1042cbc5ac1821fce4fb83d715532d3ecccad97e
harukasan <harukasan@pixiv.com>
2014-04-14 20:01:26 +0900

    remove specific attributes from default attributes

--- lib/fluent/plugin/in_rss.rb
+++ lib/fluent/plugin/in_rss.rb
@@ -7,7 +7,7 @@ module Fluent
     config_param :tag, :string
     config_param :url, :string
     config_param :interval, :time, default: '5m'
-    config_param :attrs, :string, default: 'about, date, title, description, link, content_encoded'
+    config_param :attrs, :string, default: 'date, title, description'
 
     def configure(conf)
       super
